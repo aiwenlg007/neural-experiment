b7520171b95265af51d5ec888364b81d910ea723
Rajat Pundir <rajatpundir13@gmail.com>
2017-08-27 21:26:35 +0530

    Experiment successful

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-***ARTIFICIAL NEURAL NETWORK LIBRARY***
+***NEURAL EXPERIMENT***
 
-This is an ANN library to quickly prototype my own neural net designs.
+This is an Artificial Neural Network to classify MNIST Handwritten Digits dataset, written in C language.
--- neural.c
+++ neural.c
@@ -3,16 +3,15 @@
 #include <math.h>
 #include <malloc.h>
 
-#define LEARNING_RATE	0.3
+#define LEARNING_RATE	9
 #define MATRIX_MODE	1
 #define LIST_MODE	0
 #define SHOW_STATS	1
 
 /* 
    Note to self.
-   1. an inverse activation function
-   2. logic to reverse flow of things to know neural net's viewpoint
-
+   Compile the program using following command: 
+	gcc neural.c -lm -O2 && ./a.out
 */
 
 typedef struct neuron {
@@ -31,6 +30,7 @@ typedef struct synapse {
 
 typedef struct layer {
   int neuron_count;
+  double bias;
   double *output_matrix;
   double *error_matrix;
   double **weight_matrix;
@@ -56,15 +56,12 @@ double sigmoid(double x) {
   return 1 / (1 + exp(-x));
 }
 
-double activation_function(double x) {
-  // some other alternatives can be hyperbolic tangent, softmax function, or rectifier function.
-  // maybe a switch case here, which accepts type of activation function to use.
-  return sigmoid(x);
+double derivative_of_sigmoid(double x) {
+  return sigmoid(x) * (1 - sigmoid(x));
 }
 
-double gradient_of_activaton_function(double error_of_synapse_from_back_layer, double output_of_synapse_from_layer, double output_of_synapse_to_front_layer) {
-  // currently only for sigmoid/logistic functon
-  return -1 * learning_rate() * error_of_synapse_from_back_layer   * sigmoid(output_of_synapse_to_front_layer) * sigmoid(1 - output_of_synapse_to_front_layer) * output_of_synapse_from_layer;
+double activation_function(double x) {
+  return sigmoid(x);
 }
 
 void display_weights_forward(layer *temp_layer, neuron *temp_neuron) {
@@ -119,8 +116,9 @@ void display_net(net *network) {
   layer *temp_layer = network -> input_layer;
   int layer_count = 1;
   while(temp_layer != NULL) {
-    printf("\nLayer %d\n", layer_count++);
-    printf("-------\n");
+    printf("\nLayer %d ", layer_count++);
+    printf("[Bias: %lf]", temp_layer -> bias);
+    printf("\n-------\n");
     display_layer(temp_layer);
     temp_layer = temp_layer -> next_layer;
   }
@@ -136,6 +134,7 @@ neuron * create_neuron() {
 layer * create_layer(int neurons_in_layer) {
   layer *temp_layer = (layer *) malloc(sizeof(layer));
   temp_layer -> neuron_count = neurons_in_layer;
+  temp_layer -> bias = 0;
   temp_layer -> output_matrix = (double *) malloc(neurons_in_layer * sizeof(double));
   temp_layer -> error_matrix = (double *) malloc(neurons_in_layer * sizeof(double));
   temp_layer -> weight_matrix = NULL;
@@ -233,10 +232,10 @@ void set_outputs_for_layer(layer *l, double *inputs) {
   }
 }
 
-void set_errors_for_layer(layer *l, double *expected) {
+void set_errors_for_layer(layer *l, double *target) {
   int temp = 0;
   while(temp != (l -> neuron_count)) {
-    (l -> error_matrix)[temp] = ((l -> output_matrix)[temp]- expected[temp]);
+    (l -> error_matrix)[temp] = 0.5 * pow((target[temp] - (l -> output_matrix)[temp]) , 2);
     temp++;
   }
 }
@@ -254,7 +253,7 @@ void propagate_using_lists(net *network, double *inputs) {
 	summation += ((*(wb[synapse_count] -> from_neuron -> output)) * (*(wb[synapse_count] -> weight)));
 	synapse_count++;
       }
-      *(temp_neuron -> output) = activation_function(summation);
+      *(temp_neuron -> output) = activation_function((temp_layer -> bias) + summation);
       temp_neuron = temp_neuron -> next_neuron;
     }
     temp_layer = temp_layer -> next_layer;
@@ -269,14 +268,14 @@ void propagate_using_matrices(net *network, double *inputs) {
       double summation = 0;
       for(int j = 0; j < (temp_layer -> previous_layer -> neuron_count); j++)
 	summation += ((temp_layer -> previous_layer -> output_matrix)[j] * (temp_layer -> previous_layer -> weight_matrix)[j][i]);
-      (temp_layer -> output_matrix)[i] = activation_function(summation);
+      (temp_layer -> output_matrix)[i] = activation_function((temp_layer -> bias) + summation);
     }
     temp_layer = temp_layer -> next_layer;
   }
 }
 
-void backpropagate_using_lists(net *network, double *expected) {
-  set_errors_for_layer(network -> output_layer, expected);
+void backpropagate_using_lists(net *network, double *target) {
+  set_errors_for_layer(network -> output_layer, target);
   layer *temp_layer = (network -> output_layer) -> previous_layer;
   while(temp_layer != NULL) {
     // calculate total weight going into each neuron of next layer
@@ -305,17 +304,17 @@ void backpropagate_using_lists(net *network, double *expected) {
     // update weights
     temp_neuron = temp_layer -> neurons;
     while(temp_neuron != NULL) {
-      synapse **wf = temp_neuron -> weights_forward;
+      //synapse **wf = temp_neuron -> weights_forward;
       for(int i = 0; i < (temp_layer -> next_layer -> neuron_count); i++)
-	*(wf[i] -> weight) = *(wf[i] -> weight) - learning_rate() * gradient_of_activaton_function( *(wf[i] -> from_neuron -> error), *(wf[i] -> from_neuron -> output), *(wf[i] -> to_neuron -> output));
-      temp_neuron = temp_neuron -> next_neuron;
+	//*(wf[i] -> weight) = *(wf[i] -> weight) - learning_rate() * gradient_of_activaton_function( *(wf[i] -> from_neuron -> error), *(wf[i] -> from_neuron -> output), *(wf[i] -> to_neuron -> output));
+	temp_neuron = temp_neuron -> next_neuron;
     }
     temp_layer = temp_layer -> previous_layer;
   }
 }
 
-void backpropagate_using_matrices(net *network, double *expected) {
-  set_errors_for_layer(network -> output_layer, expected);
+void backpropagate_using_matrices(net *network, double *target) {
+  set_errors_for_layer(network -> output_layer, target);
   layer *temp_layer = (network -> output_layer) -> previous_layer;
   while(temp_layer != NULL) {
     // backpropagate errors
@@ -329,10 +328,17 @@ void backpropagate_using_matrices(net *network, double *expected) {
       }
     }
     // update weights
-    for(int i = 0; i < (temp_layer -> neuron_count); i++)
-      for(int j = 0; j < (temp_layer -> next_layer -> neuron_count); j++)
-	//(temp_layer -> weight_matrix)[i][j] += (learning_rate() * gradient_of_activaton_function((temp_layer -> next_layer -> error_matrix)[i], (temp_layer -> next_layer -> output_matrix)[i], (temp_layer -> output_matrix)[j]));
-	(temp_layer -> weight_matrix)[i][j] += (learning_rate() * gradient_of_activaton_function((temp_layer -> error_matrix)[i], (temp_layer -> output_matrix)[i], (temp_layer -> next_layer -> output_matrix)[j]));
+    for(int i = 0; i < (temp_layer -> next_layer -> neuron_count); i++)
+      for(int j = 0; j < (temp_layer -> neuron_count); j++) {
+	/* printf("\n%lf -> \n", (temp_layer -> weight_matrix)[j][i]); */
+	/* printf("1. %lf\n", ((temp_layer -> next_layer -> output_matrix)[i] - target[i])); */
+	/* printf("2. %lf\n", derivative_of_sigmoid((temp_layer -> next_layer -> output_matrix)[i])); */
+	/* printf("3. %lf\n", (temp_layer -> weight_matrix)[j][i]); */
+	/* printf("4. %lf \n", (((temp_layer -> next_layer -> output_matrix)[i] - target[i]) * derivative_of_sigmoid((temp_layer -> next_layer -> output_matrix)[i]) * (temp_layer -> output_matrix)[j])); */
+	/* printf("5. %lf\n", (temp_layer -> output_matrix)[j]); */
+	(temp_layer -> weight_matrix)[j][i] -= learning_rate() *(((temp_layer -> next_layer -> output_matrix)[i] - target[i]) * derivative_of_sigmoid((temp_layer -> next_layer -> output_matrix)[i]) * (temp_layer -> output_matrix)[j]);
+	//printf("-> %lf\n", (temp_layer -> weight_matrix)[j][i]);
+      }
     temp_layer = temp_layer -> previous_layer;
   }  
 }
@@ -346,12 +352,12 @@ void propagate(net *network, double *inputs) {
   }  
 }
 
-void backpropagate(net *network, double *expected) {
+void backpropagate(net *network, double *target) {
   if(LIST_MODE) {
-    backpropagate_using_lists(network, expected);
+    backpropagate_using_lists(network, target);
   }
   else if(MATRIX_MODE) {
-    backpropagate_using_matrices(network, expected);
+    backpropagate_using_matrices(network, target);
   }
 }
 
@@ -371,16 +377,15 @@ double * get_random_inputs(net *network) {
 int get_label() {
   int num;
   fscanf(stdin, "%d,", &num);
-  //printf("\nlabel = %d\n", num);
   return num;
 }
 
-double * get_expected() {
-  double *expected = (double *) malloc(10 * sizeof(double));
+double * get_target() {
+  double *target = (double *) malloc(10 * sizeof(double));
   for(int i = 0; i < 10; i++)
-    expected[i] = 0.01;
-  expected[get_label()] = 0.99;
-  return expected;
+    target[i] = 0.01;
+  target[get_label()] = 0.99;
+  return target;
 }
 
 double * get_inputs() {
@@ -408,10 +413,10 @@ int query(net *network) {
   if(SHOW_STATS) {
     printf("\nlabel = %d -> %d\n", label, max_label);
     if(SHOW_STATS) {
-       if(label == max_label)
-	 printf("CORRECT\n");
-       else
-      printf("INCORRECT\n");
+      if(label == max_label)
+	printf("CORRECT\n");
+      else
+	printf("INCORRECT\n");
     }
     for(int i = 0; i < (network -> output_layer -> neuron_count); i++)
       printf("%d -> %.0lf\n", i, (network -> output_layer -> output_matrix)[i] * 100);
@@ -424,40 +429,61 @@ int query(net *network) {
 }
 
 void train(net *network) {
-  double *expected = get_expected();
+  double *target = get_target();
   double *inputs = get_inputs();
   propagate(network, inputs);
-  backpropagate(network, expected);
-  // experiments
-  //puts("");
-  //display_output_matrix(network -> output_layer);
-  free(expected);
+  backpropagate(network, target);
+  free(target);
   free(inputs);
+}
+
+void help_me_debug(net *network) {
+  add_layer_to_net(network, 2);
+  add_layer_to_net(network, 2);
+  (network -> output_layer -> bias) = 0.35;
+  add_layer_to_net(network, 2);
+  (network -> output_layer -> bias) = 0.60;
+  double **w = (network -> input_layer -> weight_matrix);
+  w[0][0] = 0.15;
+  w[0][1] = 0.20;
+  w[1][0] = 0.25;
+  w[1][1] = 0.30;
+  w = (network -> input_layer -> next_layer -> weight_matrix);
+  w[0][0] = 0.40;
+  w[0][1] = 0.45;
+  w[1][0] = 0.50;
+  w[1][1] = 0.55;
+  double inputs[2] = {0.05, 0.10};
+  propagate(network, inputs);
+  double target[2] = {0.01, 0.99};
+  for(int i = 0; i < 10; i++) 
+    backpropagate(network, target);
+  propagate(network, inputs);
+  display_net(network);
 }
 
 int main() {
   net *network = create_network();
 
   construct_network_architecture(network, 784, 100, 10);
-  
-  int t = 10;
-  int q = 1200;
+    
+  int train_cases = 10;
+  int test_cases = 10;
+  int epoch = 20;
   double score = 0;
   
   // train the network
-  freopen("mnist_train.csv", "r", stdin);
-  for(int i = 0; i < t; i++) {
-    train(network);
-    //display_error_matrix(network -> output_layer);
-    //display_output_matrix(network -> output_layer);
+  for(int j = 0; j < epoch; j++) {
+    freopen("mnist_test_10.csv", "r", stdin);
+    for(int i = 0; i < train_cases; i++)
+      train(network);
   }
-     
+      
   // query the network
-  freopen("mnist_test.csv", "r", stdin);
-  for(int i = 0; i < q; i++) {
+  freopen("mnist_test_10.csv", "r", stdin);
+  for(int i = 0; i < test_cases; i++)
     score += query(network);
-  }
-  printf("\nAccuracy = %lf", (score / q * 100));
-  
+  printf("\nAccuracy = %lf", (score / test_cases * 100));
+    
   return 0;
 }
